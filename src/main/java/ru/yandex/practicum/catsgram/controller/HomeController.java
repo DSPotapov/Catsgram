24d8be61edea01aeee3feb3cfe5ad088077c1dff
Денис Потапов <p0tapovdenis@yandex.ru>
2024-09-17 22:12:27 +0300

    update properties

--- src/main/java/ru/yandex/practicum/catsgram/controller/HomeController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/HomeController.java
@@ -1,15 +1,18 @@
 package ru.yandex.practicum.catsgram.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/home")
 public class HomeController {
 
     @GetMapping
     public String homePage() {
+        log.info("Страница приветствия");
         return "<h1>Приветствуем вас, в приложении Котограм<h1>";
     }
 }
